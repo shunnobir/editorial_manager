59c915771b68fba52fbd4748d448f3c88713e765
shunnobir <raihankabir20701004@gmail.com>
2024-10-03 21:48:18 +0600

    edited baseUrl to fix deployed version's CORS error

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const Axios = axios.create({
-  baseURL: "http://localhost:5000/api/",
-  // baseURL: "http://bike-csecu.com:5000/api/",
+  // baseURL: "http://localhost:5000/api/",
+  baseURL: "http://bike-csecu.com:5000/api/",
   timeout: 10000,
 });
